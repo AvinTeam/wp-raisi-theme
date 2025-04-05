1cdb71413cfd6871532ea37c758fdb2cf188cbe1
Mohammadreza <rashidpour1995@gmail.com>
2025-04-05 16:23:46 +0330

    responsive home media

--- views/home/media.php
+++ views/home/media.php
@@ -1,12 +1,12 @@
 <div class="container-fluid">
-    <div class="row">
+    <div class="row row-gap-lg-0 row-gap-3">
         <div class="col-12 col-lg-7">
 
-            <?php get_component('home-media-last')?>
+            <?php get_component('home/media-last')?>
 
         </div>
-        <div class="col-12 col-lg-5 d-flex flex-row justify-content-around align-items-center media_cat ">
-            <?php get_component('home-media-category')?>
+        <div class="col-12 col-lg-5 d-flex flex-row justify-content-around align-items-center media_cat py-lg-0 py-5">
+            <?php get_component('home/media-category')?>
         </div>
     </div>
 </div>
