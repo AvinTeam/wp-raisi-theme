39fda2cfc7cc1191be36b12214583899eb69fbc1
Mohammadreza <rashidpour1995@gmail.com>
2025-04-05 14:55:46 +0330

    responsive header

---
+++ components/ticker.php
@@ -0,0 +1,43 @@
+<div class="news secondary-shade-1 d-flex flex-row justify-content-around align-items-center w-100">
+    <div class="col-2 col-lg-1 d-flex flex-row align-items-center justify-content-around">
+        <span class="text-secondary-tint-2  fw-500 f-12px">آخرین مطالب</span>
+        <img class="w-12px" src="<?php echo image_url('dif-red.png') ?>">
+    </div>
+    <div class="col-10 col-lg-11 news-ticker">
+
+        <div class="ticker-container">
+
+            <?php
+                $recent_posts = new WP_Query([
+                    'posts_per_page' => 10,         // تعداد پست‌ها
+                    'post_status'    => 'publish', // فقط پست‌های منتشر شده
+                    'orderby'        => 'date',    // بر اساس تاریخ
+                    'order'          => 'DESC',    // جدیدترین اول
+                 ]);
+                $news_query = new WP_Query($recent_posts);
+
+                if ($news_query->have_posts()) {
+                    $m = 0;
+                    while ($news_query->have_posts()) {
+                        $news_query->the_post();
+
+                        if ($m) {
+                            echo '<img class="w-12px" src="' . image_url('dif.png') . '">';
+                        }
+
+                        $m++;
+                    ?>
+            <a href="<?php the_permalink(); ?>"
+                class="text-secondary-tint-1 fw-500 f-12px mx-4"><?php the_title(); ?></a>
+
+            <?php
+                }
+                    wp_reset_postdata();
+
+                }
+
+            ?>
+        </div>
+    </div>
+
+</div>
--- views/header.php
+++ views/header.php
@@ -1,35 +1,43 @@
-<div class="w-100">
+<div class="w-100 d-none d-lg-block">
     <div class="d-flex flex-row align-items-center justify-content-between p-24px h-88px">
         <div class="col-7 text-secondary-tint-1 f-20px fw-bold "><?php echo get_header_title() ?></div>
         <div class="col-5 d-flex flex-row align-items-center gap-4 justify-content-end">
 
             <?php
-            get_component('social-media');
-            get_search_form();
-        ?>
+                get_component('social-media');
+                get_search_form();
+            ?>
         </div>
     </div>
-    <div class="news secondary-shade-1 d-flex flex-row justify-content-around align-items-center">
-        <div class=" col-lg-1 d-flex flex-row align-items-center justify-content-around">
-            <span class="text-secondary-tint-2  fw-500 f-12px">آخرین مطالب</span>
-            <img class="w-12px" src="<?php echo image_url('dif-red.png') ?>">
-        </div>
-        <div class="col-lg-11 news-ticker">
+    <?php get_component('ticker'); ?>
+</div>
 
-            <div class="ticker-container">
-                <span class="text-secondary-tint-1 fw-500 f-12px mx-4">خبر اول: راه‌اندازی جدیدترین محصول شرکت</span>
-                <img class="w-12px" src="<?php echo image_url('dif.png') ?>">
-                <span class="text-secondary-tint-1 fw-500 f-12px mx-4">خبر دوم: برگزاری کنفرانس فناوری اطلاعات</span>
-                <img class="w-12px" src="<?php echo image_url('dif.png') ?>">
-                <span class="text-secondary-tint-1 fw-500 f-12px mx-4">خبر سوم: تغییرات جدید در قوانین مالیاتی</span>
-                <img class="w-12px" src="<?php echo image_url('dif.png') ?>">
-                <span class="text-secondary-tint-1 fw-500 f-12px mx-4">خبر چهارم: پیشنهادات ویژه برای مشتریان
-                    وفادار</span>
-            </div>
+<div class="w-100 d-flex flex-column justify-content-center align-items-center d-lg-none">
+    <a class="w-50" href="<?php echo home_url() ?>"><img class="w-100 p-16px" src="<?php echo image_url('logo.png') ?>"
+            alt="<?php echo get_bloginfo('name') ?>"></a>
+    <?php get_component('ticker'); ?>
+
+
+    <div class="d-flex flex-row align-items-center justify-content-between p-24px h-88px w-100">
+        <div class="col-1 d-flex flex-row align-items-center justify-content-center">
+            <button class="btn d-lg-none" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasSidebar">
+                <img class="h-40px w-40px" src="<?=image_url('menu.png')?>" alt="menu">
+            </button>
         </div>
 
+        <div class="col-10 text-secondary-tint-1 f-20px fw-bold text-center "><?php echo get_header_title() ?></div>
+
+        <div class="col-1 d-flex flex-row align-items-center gap-4 justify-content-end">
+
+            <?php
+                get_search_form();
+            ?>
+        </div>
     </div>
-<button class="btn btn-primary d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasSidebar">
-    منو
-</button>
+
+
+
+
+
 </div>
