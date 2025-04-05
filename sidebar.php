1d5df33c02887d70aea9c5923e4b6e21f325aae9
Mohammadreza <rashidpour1995@gmail.com>
2025-04-05 15:26:35 +0330

    responsive sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,12 +1,8 @@
 <div class="d-none d-lg-block text-center secondary-shade-4 h-100 sidebar " >
-    <div>
+    <a href="<?php echo home_url()?>">
         <img class="w-100 p-16px" src="<?php echo image_url('logo.png') ?>" alt="<?php echo get_bloginfo('name') ?>">
-    </div>
-
+    </a>
     <div class="w-100 border-1 border-secondary"></div>
-
-
-
     <nav class="sidebar-menu p-24px">
         <?php
             wp_nav_menu([
@@ -24,16 +20,9 @@
 
 
 
-
-
-
-
-
-<!-- Offcanvas سایدبار برای موبایل -->
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasSidebar">
     <div class="offcanvas-header">
-        <img class="w-100 p-16px" src="<?php echo image_url('logo.png') ?>" alt="<?php echo get_bloginfo('name') ?>">
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+        <button type="button" class="btn-close fw-heavy" data-bs-dismiss="offcanvas"><i class="bi bi-x-lg text-white f-20px"></i></button>
     </div>
     <div class="offcanvas-body">
         <nav class="sidebar-menu p-24px">
