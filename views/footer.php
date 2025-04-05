013bbea29f8eb909a63a60ecd06ba2fc58847d21
Mohammadreza <rashidpour1995@gmail.com>
2025-04-05 15:06:31 +0330

    responsive footer

--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,9 @@
-<footer class="container-fluid d-flex flex-row align-items-center p-24px secondary-shade-4 rounded-12px">
-    <img class="h-88px p-16px" src="<?php echo image_url('logo.png') ?>" alt="<?php echo get_bloginfo('name') ?>">
+<footer class="container-fluid d-flex flex-lg-row flex-column  align-items-center p-24px secondary-shade-4 rounded-12px">
+    <a href="<?=home_url()?>"><img class="h-88px p-16px" src="<?php echo image_url('logo.png') ?>" alt="<?php echo get_bloginfo('name') ?>"></a>
     <div class="w-100">
-        <div class="d-flex flex-row justify-content-between align-items-center">
+        <div class="d-flex flex-lg-row flex-column  justify-content-between align-items-center">
             <div>
+
                 <nav class="footer-menu">
                     <?php
                             wp_nav_menu([
@@ -18,8 +19,8 @@
             <div class="text-secondary-tint-1 fw-500 f-14px"> شماره تماس: <span>۹۸۲۱۸۸۷۰۵۲۸۶+</span></div>
         </div>
         <hr class="text-secondary">
-        <div class="d-flex flex-row justify-content-between align-items-center">
-            <div class="fw-500 f-10px text-secondary-tint-1 ">
+        <div class="d-flex flex-lg-row flex-column-reverse  justify-content-between align-items-center">
+            <div class="fw-500 f-10px text-secondary-tint-1 mt-3 mt-lg-0 ">
                 تمامی حقوق مادی و معنوی این سایت متعلق به بنیاد شهید رئیسی است و استفاده از مطالب با ذکر منبع بلامانع
                 است.
             </div>
